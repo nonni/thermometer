cc4c745d32a0c928d487e78d83c41a5666bca4e2
Jón B. Stefansson <nonnsi@gmail.com>
2011-07-27 16:08:20 +0000

    Working on temperature query function

---
+++ query.py
@@ -0,0 +1,22 @@
+import pymongo
+
+def get_temperature(lat, lon, date, db_host, db_name, db_port=27017):
+    """
+    Get temperature values from hottub mongodb.
+    """
+    connection = pymongo.Connection(db_host, db_port)
+    db = connection[db_name]
+    
+    for stat in db.stations.find({'loc': {'$near': [lat, lon]}}).limit(10):
+        obs = db.observations.find_one({'station': stat['usaf'], 'date': date})
+        if obs:
+            return {
+                'temp': obs['temp'],
+                'max_temp': obs['max_temp'],
+                'min_temp': obs['min_temp'],
+                'lat': stat['loc']['lat'],
+                'lon': stat['loc']['long'],
+                'usaf': stat['usaf'],
+                'station_name': stat['station_name']
+            }
+    return None
